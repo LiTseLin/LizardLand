0d141d742f9d15290ddc99ef878ae62bf467d338
LiTseLin <tachinori@smail.nchu.edu.tw>
2020-01-08 10:52:16 +0800

    20200108

---
+++ js/preloader.js
@@ -0,0 +1,10 @@
+(function($) {
+    /* ==============================================
+       LOADER -->
+        =============================================== */
+
+    jq1124(window).load(function() {
+        jq1124("#preloader").delay(800).fadeOut();
+        jq1124(".preloader").delay(1000).fadeOut("slow");
+    });
+})(jQuery);
